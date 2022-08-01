2654d73ec1d66341470e8c1ac9184acfb08df8c6
Becka7 <82467937+Becka7@users.noreply.github.com>
2022-08-01 08:51:40 +0300

    add js solution

--- FirstBadVersion/JavascriptSolution.js
+++ FirstBadVersion/JavascriptSolution.js
@@ -3,9 +3,10 @@ var solution = function(isBadVersion) {
     return function(n) {
         var left = 0;
         var right = n+1;
-        const piv = Math.floor((right+left)/2);
+        
 
         while(left <= right){
+            const piv = Math.floor((right+left)/2);
             if(isBadVersion(piv)){
                 return piv;
             }if(isBadVersion())
